790230e3fb582355b005c4395effb6ad405c33ea
Saado_Nasr <83326797+SaadoNasr@users.noreply.github.com>
2021-06-26 23:24:14 +0300

    Add files via upload

---
+++ first_bot.py
@@ -0,0 +1,10 @@
+from telegram.ext import Updater ,CommandHandler
+
+
+updater=Updater(token='1705244162:AAHk9lExIZrd4oEgd6oDH50QWyBVSsaEBh8')
+dispatcher=updater.dispatcher
+def start(boot,update):
+    bot.sendMessage(chat_id=update.Message.chat_id,text='welcom to cute bot')
+    start_handler=CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+    updater.start_polling()
