24ccc9762d0296d88591294bc855021dd451716c
Saado_Nasr <83326797+SaadoNasr@users.noreply.github.com>
2021-06-26 23:57:41 +0300

    Update first_bot.py

--- first_bot.py
+++ first_bot.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater ,CommandHandler
 
 updater=Updater(token='1705244162:AAHk9lExIZrd4oEgd6oDH50QWyBVSsaEBh8')
 dispatcher=updater.dispatcher
-def start(boot,update):
+def start(bot,update):
     bot.sendMessage(chat_id=update.Message.chat_id,text='welcom to cute bot')
     start_handler=CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
